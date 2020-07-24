100665276310d6acfb9346206940878874139bd6
animyrch <info@traductionenturc.fr>
2020-07-24 22:20:43 +0200

    Removed boilet plate code from app.js. Created basic structure for about me page. Inserted strings from an external json file. Inserted photo as static asset.

--- emircv/src/App.js
+++ emircv/src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import strings from './assets/strings.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	render() {
+		return (
+			<div className="App p-3">
+				<header>
+					<nav className="flex justify-between">
+						<div className="contact-button">Contact</div>
+						<div className="menu-icon">Menu</div>				
+					</nav>
+					<nav className="menu hidden">
+						<div>About Me</div>
+						<div>Work Experience</div>
+						<div>Education</div>
+						<div>Skills</div>
+						<div>Contact Me</div>
+					</nav>
+				</header>
+				<img src={require('./assets/myPhoto.jpg')} alt="me" className="w-1/4 m-auto" />
+				<div className="my-name">{strings.content.name}</div>
+				<div className="personal-summary">{strings.content.personal}</div>
+				<div className="mission-statement">{strings.content.mission}</div>
+				<footer className="flex content-center">
+					<div className="previous-page w-1/3">{strings.navigation.previous}</div>
+					<div className="page-number w-1/3">1/4</div>
+					<div className="next-page w-1/3">{strings.navigation.next}</div>
+				</footer>
+			</div>
+		);
+	}
 }
+
+
+
 
 export default App;
