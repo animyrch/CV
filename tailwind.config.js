0fce3149324b56a6ece1025c13cc6962134478d1
animyrch <info@traductionenturc.fr>
2020-08-03 00:58:08 +0200

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,13 @@ module.exports = {
 		extend: {
 			lineHeight: {
 				'tight': '1.15'
+			},
+			margin: {
+				'72': '18rem',
+				'80': '20rem',
+				'88': '22rem',
+				'92': '23rem',
+				'96': '24rem'
 			}
 		}
     },
